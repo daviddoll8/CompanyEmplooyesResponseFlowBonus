cc841e3e105ded6f2dbd52e7c8503c1da379e2f1
David Doll <doll.david0395@gmail.com>
2023-09-20 03:09:53 -0500

    Bonus implementation using API response flow for returning error messages

---
+++ CompanyEmployees/Entities/Responses/ApiResponseOk.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Entities.Responses
+{
+    public sealed class ApiOkResponse<TResult> : ApiBaseResponse
+    {
+        public TResult Result { get; set; }
+
+        public ApiOkResponse(TResult result) : base(true)
+        {
+            Result = result;
+        }
+    }
+}
---
+++ CompanyEmployees/Service.Contracts/ICompanyService.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Responses;
+using Shared.DataTransferObjects;
+
+namespace Service.Contracts;
+
+public interface ICompanyService
+{
+    ApiBaseResponse GetAllCompanies(bool trackChanges);
+    ApiBaseResponse GetCompany(Guid companyId, bool trackChanges);
+}
